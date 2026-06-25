f7ac0ab3870e4da63ae12ab31ab955c7bcf2a84a
maida209 <156239574+maida209@users.noreply.github.com>
2026-06-25 14:43:29 -0500

    Display JSON response from Smoothiefroot API
    Change output to display JSON response instead of type.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.write(type(smoothiefroot_response))
+st.json(smoothiefroot_response.json())
 st.json(smoothiefroot_response)
 
 # Write directly to the app
